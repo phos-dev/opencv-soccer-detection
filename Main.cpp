a86e3667c402380c16bd1442a3661cc8a8c5e0e1
Pedro Henrique <p.silva@coodesh.com>
2023-02-10 01:02:54 -0300

    chore: add dilate and erode

--- Main.cpp
+++ Main.cpp
@@ -33,40 +33,40 @@ int main()
         cv::Vec3f vec_;
         split(frame, rgb);
         Mat image = frame;
-        Mat grad_x, grad_y, abs_grad_x, abs_grad_y, gray, grad;
+        Mat grad_x, grad_y, abs_grad_x, abs_grad_y, gray, grad, binaryImg;
 
         cvtColor(image, gray, COLOR_RGB2GRAY);
+        adaptiveThreshold(gray, binaryImg, 128, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 7, 15);
         
-        
-        Sobel(gray, grad_x, CV_64F, 0, 1);
-        Sobel(gray, grad_y, CV_64F, 1, 0);
+        Sobel(gray, grad_x, CV_64F, 0, 1, 3);
+        Sobel(gray, grad_y, CV_64F, 1, 0, 3);
         
     	convertScaleAbs(grad_x, abs_grad_x);
         convertScaleAbs(grad_y, abs_grad_y);
-		addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
+		addWeighted(abs_grad_x, 2, abs_grad_y, 2, 0, grad);
 
-        Mat threshold_image = grad;
+        Mat threshold_image;
 
-        for (int y = 0;y < grad.rows;y++)
+        threshold(grad, threshold_image, 200, 255, THRESH_BINARY);
+        dilate(threshold_image, threshold_image, getStructuringElement(MORPH_RECT, Size(3, 3)));
+        erode(threshold_image, threshold_image, getStructuringElement(MORPH_RECT, Size(3, 3)));
+
+        Mat edges, dst;
+        filter2D(threshold_image, dst, -1, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+        /*Find the edges in the image using canny detector
+        Canny(binaryImg, edges, 50, 200);
+        // Create a vector to store lines of the image
+        std::vector<Vec4i> lines;
+        // Apply Hough Transform
+        HoughLinesP(edges, lines, 1, CV_PI / 180, 1, 10, 250);
+
+        for (size_t i = 0; i < lines.size(); i++)
         {
-            for (int x = 0;x < grad.cols;x++)
-            {
-                Vec3b& color = threshold_image.at<Vec3b>(y, x);
-                if (color[1] > color[2] > color[0])
-                {
-                    color[0] = color[1] = color[2] = 0;
-                }
-                else
-                {
-                    color[0] = color[1] = color[2] = 255;
-                }
-
-                threshold_image.at<Vec3b>(Point(x, y)) = color;
-            }
+            Vec4i l = lines[i];
+            line(threshold_image, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 0), 3, LINE_AA);
         }
-
-
-    	imshow("Soccer Match", threshold_image);
+        */
+        imshow("Socacer Match", dst);
 
         if (waitKey(30) == 27) // Wait for 'esc' key press to exit
         {
