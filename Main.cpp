84d6df66dae617da65ec59d0ebe182d980a71607
Pedro Henrique <p.silva@coodesh.com>
2023-02-14 21:02:30 -0300

    chore: add final

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,18 @@
 
 #include <opencv2/opencv.hpp>
 
+using namespace std;
 using namespace cv;
 
+void detectLines(const Mat& image, vector<Vec4i>& lines)
+{
+    GaussianBlur(image, image, Size(5, 5), 0);
+    
+    Mat edges;
+    Canny(image, edges, 100, 150);
+
+    HoughLinesP(edges, lines, 1, CV_PI / 180, 40, 50, 5);
+}
 int main()
 {
     VideoCapture cap("data.avi");
@@ -13,64 +23,73 @@ int main()
 		return -1;
 	}
 
-	double fps = cap.get(CAP_PROP_FPS);
-
-	namedWindow("Soccer Match", WINDOW_AUTOSIZE);
-
     while (true)
     {
 
         Mat frame, src_gray, rgb[3];
-        // Mat object is a basic image container. frame is an object of Mat.
 
-        if (!cap.read(frame)) // if not success, break loop
-        // read() decodes and captures the next frame.
+        if (!cap.read(frame)) 
         {
 	        std::cout << "\n Cannot read the video file. \n";
             break;
         }
 
-        cv::Vec3f vec_;
         split(frame, rgb);
         Mat image = frame;
-        Mat grad_x, grad_y, abs_grad_x, abs_grad_y, gray, grad, binaryImg;
+        Mat grad_x, grad_y, gray, grad;
+		Mat morph_kernel = getStructuringElement(MORPH_ELLIPSE, Size(5, 5));
+		cvtColor(image, gray, COLOR_RGB2GRAY);
+       
+        Sobel(gray, grad_x, CV_32F, 1, 0);
 
-        cvtColor(image, gray, COLOR_RGB2GRAY);
-        adaptiveThreshold(gray, binaryImg, 128, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 7, 15);
+        Sobel(gray, grad_y, CV_32F, 0, 1);
+
+        magnitude(grad_x, grad_y, grad);
+        normalize(grad, grad, 0, 255, NORM_MINMAX, CV_8U);
+
+        adaptiveThreshold(gray, grad, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 3, 5);
+        threshold(grad, grad, 50, 255, THRESH_BINARY);
+
+        morphologyEx(grad, grad, MORPH_CLOSE, morph_kernel, Point(-1, -1), 2);
         
-        Sobel(gray, grad_x, CV_64F, 0, 1, 3);
-        Sobel(gray, grad_y, CV_64F, 1, 0, 3);
-        
-    	convertScaleAbs(grad_x, abs_grad_x);
-        convertScaleAbs(grad_y, abs_grad_y);
-		addWeighted(abs_grad_x, 2, abs_grad_y, 2, 0, grad);
+        vector<Vec4i> lines;
+        detectLines(grad, lines);
 
-        Mat threshold_image;
-
-        threshold(grad, threshold_image, 200, 255, THRESH_BINARY);
-        dilate(threshold_image, threshold_image, getStructuringElement(MORPH_RECT, Size(3, 3)));
-        erode(threshold_image, threshold_image, getStructuringElement(MORPH_RECT, Size(3, 3)));
-
-        Mat edges, dst;
-        filter2D(threshold_image, dst, -1, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-        /*Find the edges in the image using canny detector
-        Canny(binaryImg, edges, 50, 200);
-        // Create a vector to store lines of the image
-        std::vector<Vec4i> lines;
-        // Apply Hough Transform
-        HoughLinesP(edges, lines, 1, CV_PI / 180, 1, 10, 250);
-
-        for (size_t i = 0; i < lines.size(); i++)
+        for(size_t i = 0; i < lines.size(); i++)
         {
             Vec4i l = lines[i];
-            line(threshold_image, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 0), 3, LINE_AA);
+            line(grad, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 0, 255), 3);
         }
-        */
-        imshow("Socacer Match", dst);
 
+        GaussianBlur(grad, grad, Size(5, 5), 0);
+
+        vector<vector<Point>> contours;
+        findContours(grad, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+        vector<vector<Point> > players;
+
+        Mat filteredImage = Mat::zeros(grad.size(), CV_8UC1);
+
+        for (int i = 0; i < contours.size(); i++)
+        {
+            double area = contourArea(contours[i]);
+            double perimeter = arcLength(contours[i], true);
+            double ratio = area / perimeter;
+            if (ratio > 1.3 && ratio < 10)
+            {
+                players.push_back(contours[i]);
+            }
+        }
+
+
+        Mat result = image.clone();
+        drawContours(result, players, -1, Scalar(0, 0, 255), 2);
+        
+        imshow("Soccer Match", result);
+        
         if (waitKey(30) == 27) // Wait for 'esc' key press to exit
         {
-            break;
+        	break;
         }
     }
 
